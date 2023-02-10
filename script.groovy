f42e05f5f58f6a8ba527443e0634338df6b474b4
mksmvskv <latetoolate@gmail.com>
2023-02-10 13:48:26 +0300

    fix

--- script.groovy
+++ script.groovy
@@ -1,3 +1,7 @@
-def build_container() {
-
+def build_container(version) {
+    withCredentials([usernamePassword(credentialsId: 'docker_repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t mksmvskv/mynginx:${VERSION} ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push mksmvskv/mynginx:${VERSION}"
+    }
 }
