3510dc3d79ab386a327151f703549cf4e1589acc
mksmvskv <latetoolate@gmail.com>
2023-02-11 13:31:40 +0300

    !

--- script_func.groovy
+++ script_func.groovy
@@ -1,8 +1,8 @@
 def builddocker() {
     withCredentials([usernamePassword(credentialsId: 'docker_repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t mksmvskv/mynginx:${param.VERSION} ."
+        sh "docker build -t mksmvskv/mynginx:6 ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push mksmvskv/mynginx:${param.VERSION}"
+        sh "docker push mksmvskv/mynginx:6"
     }
 }
 return this
